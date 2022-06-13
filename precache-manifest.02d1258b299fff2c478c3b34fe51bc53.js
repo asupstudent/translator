77b57ef6ee356b54d724a84404ee1b1cdf60220c
Ilichev Eugene <37734257+asupstudent@users.noreply.github.com>
2022-06-13 23:04:30 +0300

    Add files via upload

---
+++ precache-manifest.02d1258b299fff2c478c3b34fe51bc53.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffe0582a6734e815c992d2feaca004f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e7b9a3f1d71583be3a7",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e54e8307567cafb247e3",
+    "url": "/static/js/2.1f8d0ce3.chunk.js"
+  },
+  {
+    "revision": "3e7b9a3f1d71583be3a7",
+    "url": "/static/js/main.1a9ceace.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
